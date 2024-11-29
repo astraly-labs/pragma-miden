be45659684143960a2bdcc12bd35123c3266581f
JordyRo1 <jordy.romu@orange.fr>
2024-11-29 20:36:32 +0100

    feat: test publisher_read if pair not registered

--- crates/accounts/tests/test_publisher.rs
+++ crates/accounts/tests/test_publisher.rs
@@ -157,7 +157,6 @@ fn test_publisher_read() {
         get_entry_hash = publisher_account.code().procedures()[1].mast_root(),
         entry = word_to_masm(entry),
     );
-
     let tx_script =
         TransactionScript::compile(code, vec![], TransactionKernel::testing_assembler()).unwrap();
 
@@ -188,6 +187,104 @@ fn test_publisher_read() {
         .unwrap();
 }
 
+#[test]
+#[should_panic]
+fn test_publisher_read_fails_if_pair_not_found() {
+    //  SETUP
+    // --------------------------------------------------------------------------------------------
+    let (publisher_pub_key, _) = new_pk_and_authenticator();
+    let publisher_account_id = AccountId::try_from(10376293541461622847_u64).unwrap();
+    let entry: Word = mock_entry().try_into().unwrap();
+
+    let pair: Felt = mock_entry().pair.try_into().unwrap();
+    let mut pair_word: Word = [pair, ZERO, ZERO, ZERO];
+    // In this test we have 2 accounts:
+    // - Publisher account -> contains entries
+    // - Native account -> tries to read data from the publisher account
+    let publisher_account = PublisherAccountBuilder::new(publisher_pub_key, publisher_account_id)
+        .with_storage_slots(vec![
+            StorageSlot::Map(StorageMap::default()),
+            StorageSlot::Map(
+                StorageMap::with_entries(vec![(RpoDigest::from(pair_word), entry)]).unwrap(),
+            ),
+        ])
+        .build();
+
+    // We change pair definition
+    pair_word = [pair + Felt::new(1), ZERO, ZERO, ZERO];
+
+    let (regular_pub_key, _) = new_pk_and_authenticator();
+    let native_account = RegularAccountBuilder::new(regular_pub_key).build();
+
+    let mut mock_chain =
+        MockChain::with_accounts(&[native_account.clone(), publisher_account.clone()]);
+
+    mock_chain.seal_block(None);
+
+    let advice_inputs = get_mock_fpi_adv_inputs(&mock_chain, &publisher_account);
+    // storage read
+    let code = format!(
+        "
+        use.std::sys
+        use.miden::tx
+
+        begin
+            # push the pair stored in the map
+            push.{pair_word}
+            # => [pair_word]
+
+            # get the hash of the `get_entry` account procedure
+            push.{get_entry_hash}
+            # => [get_entry_procedure_hash, pair]
+
+            # push the foreign account id
+            push.{publisher_account_id}
+            # => [publisher_account_id, get_entry_procedure_hash, pair]
+
+            exec.tx::execute_foreign_procedure
+            # => [entry]
+
+            push.{entry} assert_eqw
+
+            # truncate the stack
+            exec.sys::truncate_stack
+        end
+        ",
+        pair_word = word_to_masm(pair_word),
+        publisher_account_id = publisher_account.id(),
+        get_entry_hash = publisher_account.code().procedures()[1].mast_root(),
+        entry = word_to_masm(entry),
+    );
+
+    let tx_script =
+        TransactionScript::compile(code, vec![], TransactionKernel::testing_assembler()).unwrap();
+
+    let tx_context = mock_chain
+        .build_tx_context(native_account.id(), &[], &[])
+        .advice_inputs(advice_inputs.clone())
+        .tx_script(tx_script)
+        .build();
+
+    let block_ref = tx_context.tx_inputs().block_header().block_num();
+
+    let mut executor: TransactionExecutor =
+        TransactionExecutor::new(Arc::new(tx_context.clone()), None).with_tracing();
+
+    // load the foreign account's code into the transaction executor
+    executor.load_account_code(publisher_account.code());
+
+    // execute the transactions.
+    // the tests assertions are directly located in the Masm script.
+    executor
+        .execute_transaction(
+            native_account.id(),
+            block_ref,
+            &[],
+            tx_context.tx_args().clone(),
+        )
+        .map_err(|e| e.to_string())
+        .unwrap();
+}
 // HELPER FUNCTIONS
 // ================================================================================================
 
