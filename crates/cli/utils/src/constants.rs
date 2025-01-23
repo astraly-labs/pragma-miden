454fb033d40af96ff86efd4c646b47ad6f45558e
JordyRo1 <jordy.romu@orange.fr>
2025-01-23 09:11:34 +0100

    fix(db): db location

--- crates/cli/utils/src/constants.rs
+++ crates/cli/utils/src/constants.rs
@@ -1,4 +1,4 @@
 pub const PRAGMA_ACCOUNTS_STORAGE_FILE: &str = "pragma_miden.json";
 pub const ORACLE_ACCOUNT_COLUMN: &str = "oracle_account_id";
 pub const PUBLISHER_ACCOUNT_COLUMN: &str = "publisher_account_id";
-pub const STORE_FILENAME: &str = "store.sqlite3";
+pub const STORE_FILENAME: &str = "local-node/store.sqlite3";
